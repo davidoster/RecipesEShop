6b378e1a5bf81ee47c1e548b5adffa22e37eba2b
David Oster aka George Pasparakis <paspa@hotmail.com>
2021-12-06 14:42:46 +0200

    MainCategoriesController Autenticated requests from the controller

---
+++ RecipesEShop/Controllers/MainCategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using RecipesEShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecipesEShop.Controllers
+{
+    public class MainCategoriesController: Controller
+    {
+        public ActionResult Index()
+        {
+            if(!Request.IsAuthenticated) return RedirectToAction("PublicCategories");
+            return RedirectToAction("PrivateCategories");
+        }
+
+        public ViewResult PublicCategories()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            return View(db.Categories.ToList());
+        }
+
+        public ActionResult PrivateCategories()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            if (!Request.IsAuthenticated) return RedirectToAction("PublicCategories");
+            else
+                return View(db.Categories.ToList());
+        }
+    }
+}
